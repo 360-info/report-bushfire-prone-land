8825e53066fea5dfde89a60d97f2c184ce82fce7
Dean Marchiori <deanmarchiori@gmail.com>
2023-11-08 09:08:26 +1100

    refactoring

--- analysis/BFP/R/200_make_australia.R
+++ analysis/BFP/R/200_make_australia.R
@@ -1,14 +1,14 @@
 #' Make all of Australia map
 #' Combines the state level processed datasets into an Australia map
 
-act <- read_sf("data/final/act.gpkg")
-nt <- read_sf("data/final/nt.gpkg")
-sa <- read_sf("data/final/sa.gpkg")
-wa <- read_sf("data/final/wa.gpkg")
-vic <- read_sf("data/final/vic.gpkg")
-tas <- read_sf("data/final/tas.gpkg")
-nsw <- read_sf("data/final/nsw.gpkg")
-qld <- read_sf("data/final/qld.gpkg")
+act <- read_sf(here("data/final/act.gpkg"))
+nt <- read_sf(here("data/final/nt.gpkg"))
+sa <- read_sf(here("data/final/sa.gpkg"))
+wa <- read_sf(here("data/final/wa.gpkg"))
+vic <- read_sf(here("data/final/vic.gpkg"))
+tas <- read_sf(here("data/final/tas.gpkg"))
+nsw <- read_sf(here("data/final/nsw.gpkg"))
+qld <- read_sf(here("data/final/qld.gpkg"))
 
 
 aus <- bind_rows(
@@ -22,7 +22,4 @@ aus <- bind_rows(
     qld
 )
 
-write_sf(aus, "data/final/aus.gpkg")
-
-
-
+write_sf(aus, here("data/final/aus.gpkg"))
